0c651ce4e40f0c13c49ca09f55127624ec8ad7b6
Paul Thomas <paulthomas2107@gmail.com>
2023-05-28 15:19:27 +0100

    More

--- main.py
+++ main.py
@@ -5,6 +5,25 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Home"
+
+@app.route("/get-user/<user_id>")
+def get_user(user_id):
+    user_data = {
+        "user_id" : user_id,
+        "name" : "Rory Thomas",
+        "email": "rory@rory.co.uk"
+    }
+    extra = request.args.get("extra")
+    if extra:
+        user_data["extra"] = extra
+        
+    return jsonify(user_data), 200
+
     
+@app.route("/create-user", methods=['POST'])
+def create_user():
+    data = request.get_json()
+    return jsonify(data), 201
+
 if __name__ == "__main__":
     app.run(debug=True)
